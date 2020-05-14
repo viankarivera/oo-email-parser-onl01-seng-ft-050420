94434bfbbd8e14c8de66248b3d6e7ab195f82e39
Vianka Rivera <viankalrivera@gmail.com>
2020-05-14 16:50:34 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,9 +11,9 @@ def initialize(emails)
 end
 
 def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')}
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
+  email_array = @email.split(/[, ]/).uniq
+    email_array.reject! {|element| element.empty?}
+    email_array
+  end
 
 end
